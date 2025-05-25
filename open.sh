92b9c696cd3da60c3f21fe0b00c95694e78508d8
Julián Villaquirá <jc.villaquira@uniandes.edu.co>
2025-05-25 23:53:45 +0300

    extend "open" functionality
    - automatically looks for "file" or "table"
    - allows for running "open" without flags and opens any downloaded file

--- open.sh
+++ open.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo $fullname > $(dirname $0)/temp
+if [[ " $@ " == *" -h "* ]]; then
+  grep -A 1 --colour "\#\+\s*$(basename $0)" $(dirname $0)/README.md
+  exit 0
+fi
 
 declare -A program=(
   [csv]="libreoffice --norestore"
@@ -12,22 +15,32 @@ declare -A program=(
   [xlsx]="libreoffice --norestore"
 )
 
-declare -A params=([f]=file)
+declare -A params
+if [[ -n $@ ]]; then
+  params[f]=file,table
+else
+  params[f]="*"
+fi
+
+modified_prefix=0
 while getopts "n:f:e:p:h" flag; do
-  if [[ $flag == h ]]; then
-    grep -A 1 --colour "\#\+\s*$(basename $0)" $(dirname $0)/README.md
-    exit 0
+  if [[ $flag == f ]]; then
+    modified_prefix=1
   fi
   params[$flag]=$OPTARG
 done
 
-basename=${params[f]}${params[n]}
-tmp_err=$(mktemp)
-fullname=$(ls downloads/*/downloads/*/$basename.* 2> $tmp_err)
-if [[ -n $(<$tmp_err) ]]; then
-  echo "File downloads/*/downloads/*/$basename.* not found."
+if [[ $modified_prefix -eq 0 && -n $@ ]]; then
+  fullname=($(eval ls downloads/*/downloads/*/{${params[f]}}${params[n]}.* 2> /dev/null))
+else
+  fullname=($(ls downloads/*/downloads/*/${params[f]}${params[n]}.* 2> /dev/null))
+fi
+
+if [[ ! -n $fullname ]]; then
+  echo "File downloads/*/downloads/*/{${params[f]}}${params[n]}.* not found."
   exit 1
 fi
+echo $fullname > $(dirname $0)/temp
 
 echo "Opening $fullname"
 filename=$(basename $fullname)
@@ -38,9 +51,8 @@ if [[ -v params[p] ]]; then
 fi
 
 if [[ ! -v program[$extension] ]]; then
-  echo "Extension '$extension' not recognized, please add it to the file $0 or specify program with the flag -p."
+  echo "Extension '$extension' not recognized, please add it to $0 or specify program with the flag -p."
   exit 1
 fi
 
-echo $fullname > $(dirname $0)/temp
 ${program[$extension]} $fullname
